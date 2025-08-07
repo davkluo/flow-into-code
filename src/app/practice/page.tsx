cbed49e3d4493d17cbfc56055c6500f1b9df8d17
David Luo <davidkluo@gmail.com>
2025-08-07 15:08:21 -0700

    Reorganize fetch helper

--- src/app/practice/page.tsx
+++ src/app/practice/page.tsx
@@ -1,29 +1,10 @@
 import { PracticeAccordionSections } from "@/components/pages/PracticeAccordionSections";
 import { Timer } from "@/components/pages/Timer";
 import { TimerProvider } from "@/context/TimerContext";
-import { lcProblemListQuery } from "@/services/leetcode/graphql";
-import { LCProblem } from "@/types/leetcode";
+import { fetchLCProblems } from "@/services/leetcode/fetchProblems";
 
 export default async function PracticePage() {
-  const res = await fetch("https://leetcode.com/graphql", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      query: lcProblemListQuery,
-      variables: {
-        categorySlug: "",
-        skip: 0,
-        limit: 5000, // LeetCode has 3625 problems as of July 2025
-        filters: {},
-      },
-    }),
-    next: { revalidate: 86400 }, // Revalidate every 24 hours
-  });
-
-  const json = await res.json();
-  const problems: LCProblem[] = json.data.problemsetQuestionList.questions;
+  const problems = await fetchLCProblems();
 
   return (
     <TimerProvider defaultTime={1800}>
