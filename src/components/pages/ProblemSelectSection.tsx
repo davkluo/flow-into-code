079857c86f1b0aa44ad98d48a0c396e35fdc3e7c
David Luo <davidkluo@gmail.com>
2025-08-12 16:43:02 -0700

    Add disclaimer for LC problems; remove LC descriptions

--- src/components/pages/ProblemSelectSection.tsx
+++ src/components/pages/ProblemSelectSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import _ from "lodash";
-import { Check, ChevronsUpDown } from "lucide-react";
+import { Check, ChevronsUpDown, ExternalLink } from "lucide-react";
 import { useEffect, useMemo, useState } from "react";
 import { FixedSizeList as List } from "react-window";
 import { DifficultyBadge } from "@/components/shared/DifficultyBadge";
@@ -136,8 +136,8 @@ export function ProblemSelectSection({
   }, [problems, debouncedSearch]);
 
   return (
-    <AccordionContent className="grid grid-cols-6 gap-4 px-3.5">
-      <p className="text-muted-foreground col-span-full text-xs">
+    <AccordionContent className="flex flex-col gap-4 px-3.5">
+      <p className="text-muted-foreground text-xs">
         Select a problem from LeetCode or enter a custom problem description.
       </p>
       <Select
@@ -145,7 +145,7 @@ export function ProblemSelectSection({
         onValueChange={setSource}
         disabled={!isEditable}
       >
-        <SelectTrigger className="col-span-1 w-full">
+        <SelectTrigger>
           <SelectValue placeholder="Select a source" />
         </SelectTrigger>
         <SelectContent>
@@ -157,85 +157,129 @@ export function ProblemSelectSection({
       </Select>
 
       {source === ProblemSource.LeetCode && (
-        <Popover open={open} onOpenChange={setOpen}>
-          <PopoverTrigger asChild>
-            <Button
-              variant="outline"
-              role="combobox"
-              className="w-[400px] justify-between"
-              disabled={!isEditable}
-            >
-              <div className="flex w-full items-center">
-                <div className="truncate text-center">
-                  {selectedProblem
-                    ? `${selectedProblem.id}. ${selectedProblem.title}`
-                    : "Select a problem"}
+        <div className="flex flex-col gap-4">
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <Button
+                variant="outline"
+                role="combobox"
+                className="w-[400px] justify-between"
+                disabled={!isEditable}
+              >
+                <div className="flex w-full items-center">
+                  <div className="truncate text-center">
+                    {selectedProblem
+                      ? `${selectedProblem.id}. ${selectedProblem.title}`
+                      : "Select a problem"}
+                  </div>
+                  {selectedProblem && (
+                    <DifficultyBadge
+                      difficulty={selectedProblem.difficulty}
+                      className="ml-2"
+                    />
+                  )}
+                  <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
                 </div>
-                {selectedProblem && (
-                  <DifficultyBadge
-                    difficulty={selectedProblem.difficulty}
-                    className="ml-2"
-                  />
-                )}
-                <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
-              </div>
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-[400px] p-0">
-            <Command shouldFilter={false}>
-              <CommandInput
-                placeholder="Search problems..."
-                value={search}
-                onValueChange={handleSearchChange}
-              />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[400px] p-0">
+              <Command shouldFilter={false}>
+                <CommandInput
+                  placeholder="Search problems..."
+                  value={search}
+                  onValueChange={handleSearchChange}
+                />
+                <div className="max-h-[300px] scroll-py-1 overflow-x-hidden overflow-y-auto">
+                  {filteredAndSortedProblems.length === 0 ? (
+                    <div className="text-muted-foreground py-6 text-center text-sm">
+                      No results found.
+                    </div>
+                  ) : (
+                    <div className="overflow-hidden p-1">
+                      <List
+                        height={300}
+                        itemCount={filteredAndSortedProblems.length}
+                        itemSize={40}
+                        width="100%"
+                      >
+                        {({ index, style }) => {
+                          const problem = filteredAndSortedProblems[index];
+                          return (
+                            <div style={style} key={problem.id}>
+                              <CommandItem
+                                onSelect={() => {
+                                  handleProblemSelect(problem);
+                                }}
+                              >
+                                <div className="truncate text-center">
+                                  {problem.id}. {problem.title}
+                                </div>
+                                <DifficultyBadge
+                                  difficulty={problem.difficulty}
+                                />
+                                <Check
+                                  className={cn(
+                                    "ml-auto",
+                                    selectedProblem === problem
+                                      ? "opacity-100"
+                                      : "opacity-0",
+                                  )}
+                                />
+                              </CommandItem>
+                            </div>
+                          );
+                        }}
+                      </List>
+                    </div>
+                  )}
+                </div>
+              </Command>
+            </PopoverContent>
+          </Popover>
 
-              <div className="max-h-[300px] scroll-py-1 overflow-x-hidden overflow-y-auto">
-                {filteredAndSortedProblems.length === 0 ? (
-                  <div className="text-muted-foreground py-6 text-center text-sm">
-                    No results found.
-                  </div>
-                ) : (
-                  <div className="overflow-hidden p-1">
-                    <List
-                      height={300}
-                      itemCount={filteredAndSortedProblems.length}
-                      itemSize={40}
-                      width="100%"
-                    >
-                      {({ index, style }) => {
-                        const problem = filteredAndSortedProblems[index];
-                        return (
-                          <div style={style} key={problem.id}>
-                            <CommandItem
-                              onSelect={() => {
-                                handleProblemSelect(problem);
-                              }}
-                            >
-                              <div className="truncate text-center">
-                                {problem.id}. {problem.title}
-                              </div>
-                              <DifficultyBadge
-                                difficulty={problem.difficulty}
-                              />
-                              <Check
-                                className={cn(
-                                  "ml-auto",
-                                  selectedProblem === problem
-                                    ? "opacity-100"
-                                    : "opacity-0",
-                                )}
-                              />
-                            </CommandItem>
-                          </div>
-                        );
-                      }}
-                    </List>
-                  </div>
-                )}
+          {selectedProblem && (
+            <div className="mt-2 flex flex-col gap-2 text-xs">
+              <div className="text-muted-foreground flex gap-1">
+                Topics:{" "}
+                {selectedProblem.topicTags.map((tag) => (
+                  <TagBadge key={tag.id} tag={tag} />
+                ))}
               </div>
-            </Command>
-          </PopoverContent>
-        </Popover>
+
+              <a
+                href={`https://leetcode.com/problems/${selectedProblem.titleSlug}/description/`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="w-fit"
+              >
+                <span className="flex items-center font-semibold hover:underline">
+                  View full description on LeetCode
+                  <ExternalLink className="ml-1.5 inline h-4 w-4" />
+                </span>
+              </a>
+            </div>
+          )}
+
+          <p className="text-muted-foreground text-xs">
+            LeetCode owns the copyright to all problems sourced from its
+            platform. This app is not affiliated with, endorsed by, or sponsored
+            by LeetCode. It does not claim ownership of any LeetCode problems
+            and does not distribute them for commercial gain. The inclusion of
+            LeetCode problems on this platform is purely due to the prevalence
+            of LeetCode problems in the coding interview space. Users are
+            encouraged to view the complete problem descriptions and execute
+            their code at{" "}
+            <a
+              href="https://leetcode.com"
+              className="underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              leetcode.com
+            </a>
+            .
+          </p>
+        </div>
       )}
 
       {source === ProblemSource.Custom && (
@@ -243,35 +287,9 @@ export function ProblemSelectSection({
           placeholder="Enter your custom problem description."
           value={customProblem}
           onChange={(e) => setCustomProblem(e.target.value)}
-          className="col-span-full h-40"
+          className="h-40"
           disabled={!isEditable}
         />
-      )}
-
-      {source === ProblemSource.LeetCode && selectedProblem && (
-        <div className="col-span-full mt-2">
-          <h3 className="mb-2 text-lg font-semibold">
-            {selectedProblem.title}
-          </h3>
-          <div className="text-muted-foreground mb-4 flex items-center gap-4 text-xs">
-            <div className="flex items-center gap-1">
-              Difficulty:{" "}
-              <DifficultyBadge difficulty={selectedProblem.difficulty} />
-            </div>
-            <div className="flex items-center gap-1">
-              Tags:{" "}
-              <div className="flex flex-wrap gap-1">
-                {selectedProblem.topicTags.map((tag) => (
-                  <TagBadge key={tag.id} tag={tag} />
-                ))}
-              </div>
-            </div>
-          </div>
-          <div
-            className="prose prose-sm text-sm [&_img]:h-auto [&_img]:max-w-full [&_img]:scale-75"
-            dangerouslySetInnerHTML={{ __html: problemDetails?.content || "" }}
-          />
-        </div>
       )}
     </AccordionContent>
   );
