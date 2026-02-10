7dbad00200b9c23dea86d3154b93408d1cc64e9e
David Luo <davidkluo@gmail.com>
2026-02-10 13:59:35 -0800

    Clean up obsolete files

--- src/app/api/process-problem/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { processProblem } from "@/services/processProblem";
-import { Problem } from "@/types/problem";
-
-export async function POST(req: Request) {
-  await processProblem((await req.json()) as Problem);
-  return NextResponse.json({ status: "processing started" });
-}
