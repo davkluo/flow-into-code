3e3e50c691c2285b5fa65efa54a0e54dbd820d9e
David Luo <davidkluo@gmail.com>
2026-02-20 14:30:45 -0800

    Make PseudocodeEditor span full height

--- src/components/pages/PseudocodeEditor.tsx
+++ src/components/pages/PseudocodeEditor.tsx
@@ -3,7 +3,6 @@
 import { EditorView } from "@codemirror/view";
 import CodeMirror from "@uiw/react-codemirror";
 import { useTheme } from "next-themes";
-import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface PseudocodeEditorProps {
   value: string;
@@ -14,17 +13,16 @@ export function PseudocodeEditor({ value, onChange }: PseudocodeEditorProps) {
   const { resolvedTheme } = useTheme();
 
   return (
-    <ScrollArea className="h-full w-full">
-      <CodeMirror
-        value={value}
-        height="100%"
-        theme={resolvedTheme === "dark" ? "dark" : "light"}
-        basicSetup={{
-          tabSize: 2,
-        }}
-        extensions={[EditorView.lineWrapping]}
-        onChange={onChange}
-      />
-    </ScrollArea>
+    <CodeMirror
+      value={value}
+      height="100%"
+      className="h-full"
+      theme={resolvedTheme === "dark" ? "dark" : "light"}
+      basicSetup={{
+        tabSize: 2,
+      }}
+      extensions={[EditorView.lineWrapping]}
+      onChange={onChange}
+    />
   );
 }
