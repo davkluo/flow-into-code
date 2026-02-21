0a1c2e3966456216b85a15f0343c7ef829758597
David Luo <davidkluo@gmail.com>
2026-02-20 22:16:31 -0800

    Resolve issue where auth does not load in time for initial authFetch

--- src/lib/authFetch.ts
+++ src/lib/authFetch.ts
@@ -1,6 +1,7 @@
 import { auth } from "./firebase";
 
 export async function authFetch(url: string, options: RequestInit = {}): Promise<Response> {
+  await auth.authStateReady();
   const token = await auth.currentUser?.getIdToken();
   return fetch(url, {
     ...options,
