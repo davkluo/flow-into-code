a09b59292d562709697c83ea194db86eb5cf7109
David Luo <davidkluo@gmail.com>
2026-02-24 12:45:04 -0800

    Remove legacy type files

--- src/types/tags.ts
+++
@@ -1,8 +0,0 @@
-import z from "zod";
-
-export const TagDocSchema = z.object({
-  id: z.string(),
-  displayName: z.string(),
-  commonHints: z.array(z.string()).default([]),
-  commonPitfalls: z.array(z.string()).default([]),
-});
